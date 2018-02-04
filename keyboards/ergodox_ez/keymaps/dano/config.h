9b429a0279b36bb1fa26c14f5a7ab2b70f6dfc4b
DanHodges <dhdgs690@gmail.com>
2018-02-03 23:25:06 -0600

    great success

---
+++ keyboards/ergodox_ez/keymaps/dano/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "config_common.h"
+
+#define ONESHOT_TAP_TOGGLE 2 /* Tapping this number of times holds the key until tapped this number of times again. */
+#define ONESHOT_TIMEOUT 3000
+#endif
--- keyboards/ergodox_ez/keymaps/dano/keymap.c
+++ keyboards/ergodox_ez/keymaps/dano/keymap.c
@@ -3,10 +3,6 @@
 #include "action_layer.h"
 #include "version.h"
 
-#include "keymap_german.h"
-
-#include "keymap_nordic.h"
-
 enum custom_keycodes
 {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -21,8 +17,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_LBRACKET,
         LT(3, KC_TAB), KC_Q, KC_W, KC_F, KC_P, KC_G, KC_LCBR,
         CTL_T(KC_ESCAPE), KC_A, KC_R, KC_S, KC_T, KC_D,
-        KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LPRN,
-        TT(1), KC_LSHIFT, KC_LCTL, KC_LALT, KC_LGUI,
+        OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LPRN,
+        TT(1), KC_LSHIFT, OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI),
         LALT(LGUI(KC_LEFT)), LALT(LGUI(KC_DOWN)),
         LALT(LGUI(KC_F)),
         GUI_T(KC_BSPACE), TT(2), LGUI(KC_SPACE),
@@ -30,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RBRACKET, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLASH,
         KC_RCBR, KC_J, KC_L, KC_U, KC_Y, KC_SCOLON, KC_MINUS,
         KC_H, KC_N, KC_E, KC_I, KC_O, RCTL_T(KC_QUOTE),
-        KC_RPRN, KC_K, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT,
+        KC_RPRN, KC_K, KC_M, KC_COMMA, KC_DOT, KC_SLASH, OSM(MOD_RSFT),
         KC_EQUAL, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
         LALT(LGUI(KC_UP)), LALT(LGUI(KC_RIGHT)),
         LGUI(KC_GRAVE),
